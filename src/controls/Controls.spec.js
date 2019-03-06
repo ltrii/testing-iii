6f16c5f9aaed2dc92f129b4dd37a5cc6f499d639
ltrii <lelandtr@gmail.com>
2019-03-06 15:18:27 -0800

    Additional tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+// Test away
+import React from 'react';
+import Controls from './Controls';
+import { cleanup, fireEvent, render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('Controls', () => {
+    test('Lock Check', () => {
+        const button1 = render(<Controls locked={true} closed={true} />).getByTestId("clopenbtn");
+        const controls = render(<Controls locked={true} closed={true} />);
+        fireEvent.click(button1);
+        const lockhold = controls.getByTestId("lockbtn");
+        expect(lockhold).toHaveTextContent('Unlock Gate');
+    })
+}
+)
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+// Test away
+import React from 'react';
+import Display from './Display';
+import { cleanup, fireEvent, render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('Display', () => {
+    test('Prop Response Check', () => {
+        const controls = render(<Display locked={true} closed={true} />);
+        const lockhold = controls.getByTestId("lockbox");
+        expect(lockhold).toHaveTextContent('Locked');
+    })
+}
+)
