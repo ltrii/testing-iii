79c8533cf3b008151dbae1ffd752c78e3ccc0bc5
ltrii <lelandtr@gmail.com>
2019-03-06 13:30:06 -0800

    Init with some tests for locking/unlocking

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,38 @@
 // Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import { cleanup, fireEvent, render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('Dashboard component', () => {
+    test('Closing Works', () => {
+        const button1 = render(<Dashboard />).getByTestId("clopenbtn");
+        const dashboard = render(<Dashboard />);
+        fireEvent.click(button1);
+        const lockhold = dashboard.getByTestId("clopenbox");
+        expect(lockhold).toHaveTextContent('Closed');
+    })
+    test('Locking Works', () => {
+        const button1 = render(<Dashboard />).getByTestId("clopenbtn");
+        const button2 = render(<Dashboard />).getByTestId("lockbtn");
+        const dashboard = render(<Dashboard />);
+        fireEvent.click(button1);
+        fireEvent.click(button2);
+        const lockhold = dashboard.getByTestId("lockbox");
+        expect(lockhold).toHaveTextContent('Locked');
+    })
+    test('Unlocking Works', () => {
+        const button1 = render(<Dashboard />).getByTestId("clopenbtn");
+        const button2 = render(<Dashboard />).getByTestId("lockbtn");
+        const dashboard = render(<Dashboard />);
+        fireEvent.click(button1);
+        fireEvent.click(button2);
+        fireEvent.click(button2);
+        fireEvent.click(button1);
+        const lockhold = dashboard.getByTestId("lockbox");
+        expect(lockhold).toHaveTextContent('Unlocked');
+    })
+})
